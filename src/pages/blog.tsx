367e2c8599f052187b4dd4845e316c3784527674
FujimuraShuhei <shushu2525.1694@gmail.com>
2022-08-13 15:11:24 +0900

    🐛 blogページ key追加

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,54 +4,70 @@ import React from 'react'
 
 const BLOG = [
   {
+    id: 1,
     title: 'This is a header',
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
     date: '2022.07.11',
   },
   {
+    id: 2,
     title: 'This is a header',
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
     date: '2022.07.11',
   },
   {
+    id: 3,
     title: 'This is a header',
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
     date: '2022.07.11',
   },
   {
+    id: 4,
     title: 'This is a header',
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
     date: '2022.07.11',
   },
   {
+    id: 5,
     title: 'This is a header',
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
     date: '2022.07.11',
   },
   {
+    id: 6,
     title: 'This is a header',
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
     date: '2022.07.11',
   },
   {
+    id: 7,
     title: 'This is a header',
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
     date: '2022.07.11',
   },
   {
+    id: 8,
     title: 'This is a header',
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
     date: '2022.07.11',
   },
   {
+    id: 9,
+    title: 'This is a header',
+    description:
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
+    date: '2022.07.11',
+  },
+  {
+    id: 10,
     title: 'This is a header',
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
@@ -66,7 +82,7 @@ const blog:React.FC = () => {
         <h2 className={styles.blog_h2}>Blog</h2>
         {BLOG.map((blog) => {
           return (
-            <div>
+            <div key={blog.id}>
               <h3 className={styles.blog_title}>{blog.title}</h3>
               <p className={styles.blog_description}>{blog.description}</p>
               <p className={styles.blog_date}>{blog.date}</p>
